03d81c2187691f4bd1c7c058ab87c934a1bb5192
ihorr <garik.rybalko@gmail.com>
2025-01-22 21:38:57 +0200

    prototype is done

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,20 +1,33 @@
 'use strict';
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
 
-// function generateDice = () => {
-//     return Math.trunc(Math.random() * 10);
-// }
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Add Roll button handler
 btnRoll.addEventListener('click', function () {
@@ -22,8 +35,39 @@ btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6 + 1);
   // 2. Display dice
   diceEl.classList.remove('hidden');
+
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    switchPlayer();
+  }
 });
 
 // btn--new
 // btn--roll
 // btn--hold
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //2. Check if player's scrore is >= 100
+  if (scores[activePlayer] >= 20) {
+    //Finish game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    // Switch to next player
+    switchPlayer();
+  }
+});
